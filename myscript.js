2a8c5e58f1d483c5cb36ec86e63329690bdcec7b
mervert04 <105776886+mervert04@users.noreply.github.com>
2022-12-20 02:53:53 +0600

    aaaaaa send help

--- myscript.js
+++ myscript.js
@@ -77,3 +77,21 @@ img.onclick = function(){
 function closeModal(id) {
   $('#'+id).fadeOut('medium');
 } 
+var mybutton = document.getElementById("myBtn");
+  
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
